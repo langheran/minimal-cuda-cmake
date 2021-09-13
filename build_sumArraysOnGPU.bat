e384f0a16a50950a8985f9373f84f738dd99d07e
langheran <langheran@gmail.com>
2021-09-13 18:16:18 -0500

    changes

--- build_sumArraysOnGPU.bat
+++ build_sumArraysOnGPU.bat
@@ -1,4 +1,8 @@
 cd ./build/Debug
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set "vcvarsdir=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
+@REM set "vcvarsdir=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
+pushd "%vcvarsdir%"
+call vcvarsall.bat x64
+popd
 cmake.exe --build . --config Debug --target all -- -j 6 && exit || pause && exit
 cd ../..
--- configure_sumArraysOnGPU.bat
+++ configure_sumArraysOnGPU.bat
@@ -3,7 +3,12 @@ mkdir build
 cd build
 mkdir Debug
 cd Debug
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set "vcvarsdir=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
+@REM set "vcvarsdir=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
+pushd "%vcvarsdir%"
+call vcvarsall.bat x64
+popd
+@REM cmake.exe -G "Visual Studio 16 2019" -A "x64" ../..
 cmake.exe -G "Ninja" ../..
 cd ../..
 pause
